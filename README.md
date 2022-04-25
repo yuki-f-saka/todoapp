c63d412b4a0723f6f77ac289c1a496f92a06eaf3
Yuki Funasaka <yuki.funasaka@uhuru.jp>
2022-04-26 00:11:42 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,44 @@
+# Name（リポジトリ/プロジェクト/OSSなどの名前）
+
+ToDo app
+
+Node.jsの勉強用にToDo管理できるようなアプリケーションを作成しました。
+
+# Demo
+
+ToDo画面
+
+
+
+<!-- # Requirement -->
+
+<!-- "hoge"を動かすのに必要なライブラリなどを列挙する
+
+* huga 3.5.2
+* hogehuga 1.0.2 -->
+
+<!-- # Installation -->
+
+<!-- Requirementで列挙したライブラリなどのインストール方法を説明する
+
+```bash
+pip install huga_package
+``` -->
+
+<!-- # Usage -->
+
+<!-- DEMOの実行方法など、"hoge"の基本的な使い方を説明する
+
+```bash
+git clone https://github.com/hoge/~
+cd examples
+python demo.py
+``` -->
+
+# Author
+
+作成情報を列挙する
+
+* Yuki Funasaka
+* uhuru
+* ffffna0lucky+github@gmail.com
---
+++ routes/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const knex = require('../db/knex');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  knex("tasks")
+    .select("*")
+    .then(function (results) {
+      console.log(results);
+      res.render('index', {
+        title: 'ToDo App',
+        todos: results,
+      });
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+      });
+    });
+});
+
+router.post('/', function (req, res, next) {
+  const todo = req.body.add;
+  knex("tasks")
+    .insert({user_id: 1, content: todo})
+    .then(function () {
+      res.redirect('/')
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+      });
+    });
+});
+
+router.use('/signup', require('./signup'));
+
+module.exports = router;
